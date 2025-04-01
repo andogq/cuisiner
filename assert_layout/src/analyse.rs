2e441454d6423c627035acc644511ff48f0abfcc
Tom Anderson <tom@ando.sh>
2025-04-01 17:24:40 +1100

    wip: `assert_layout` better support for generics

--- assert_layout/src/analyse.rs
+++ assert_layout/src/analyse.rs
@@ -1,8 +1,10 @@
-use std::ops::Deref;
+use std::{collections::HashMap, ops::Deref};
 
 use proc_macro2::Span;
 use syn::{
-    Error, Expr, GenericArgument, GenericParam, Ident, ItemStruct, Member, Type, parse_quote,
+    Error, Expr, GenericArgument, GenericParam, Ident, ItemStruct, Member, Path, PathArguments,
+    PathSegment, Token, Type, TypeParamBound, TypePath, parse_quote, parse_quote_spanned,
+    punctuated::Punctuated, spanned::Spanned,
 };
 
 use crate::parse::Ast;
@@ -28,8 +30,27 @@ pub fn analyse(ast: Ast) -> Result<LayoutModel, Error> {
 
     // Build the container type.
     let container_ident = ast.item.ident.clone();
-    let container_generics = ast.generics.iter().flat_map(|generic| generic.deref());
-    let container_type: Type = parse_quote!(#container_ident<#(#container_generics,)*>);
+    let container_generics = ast.item.generics.split_for_impl().1;
+    let container_type: Type = parse_quote!(#container_ident #container_generics);
+
+    // Collect all container generics which are only bound by a single trait in order to
+    // disambiguate them.
+    let bound_generics = ast
+        .item
+        .generics
+        .type_params()
+        .filter_map(|param| {
+            if param.bounds.len() != 1 {
+                return None;
+            };
+
+            let Some(TypeParamBound::Trait(bound)) = param.bounds.first() else {
+                return None;
+            };
+
+            Some((param.ident.clone(), bound.path.clone()))
+        })
+        .collect::<HashMap<_, _>>();
 
     // Zip up provided generics with container generics.
     let generics = ast
@@ -82,7 +103,7 @@ pub fn analyse(ast: Ast) -> Result<LayoutModel, Error> {
                 .flat_map(|assertion| {
                     [
                         assertion.size.map(|size| Assertion::Size {
-                            ty: assertion.ty,
+                            ty: qualify_generic(&bound_generics, assertion.ty),
                             size,
                         }),
                         assertion.offset.map(|offset| Assertion::Offset {
@@ -124,4 +145,164 @@ pub enum Assertion {
 pub enum AssertionItem {
     Const { ident: Ident, ty: Type, expr: Expr },
     Type { ident: Ident, ty: Type },
+}
+
+/// Traverse all of the path segments, and qualify any nested generics.
+fn qualify_path_segments<'a>(
+    bound_generics: &'a HashMap<Ident, Path>,
+    segments: impl Iterator<Item = &'a mut PathSegment>,
+) {
+    segments.for_each(|segment| {
+        match &mut segment.arguments {
+            PathArguments::None => {}
+            PathArguments::AngleBracketed(args) => {
+                args.args
+                    .iter_mut()
+                    .filter_map(|arg| {
+                        if let GenericArgument::Type(ty) = arg {
+                            Some(ty)
+                        } else {
+                            None
+                        }
+                    })
+                    .for_each(|ty| {
+                        *ty = qualify_generic(
+                            bound_generics,
+                            // Sad clone :(
+                            ty.clone(),
+                        );
+                    });
+            }
+            PathArguments::Parenthesized(args) => {
+                args.inputs.iter_mut().for_each(|ty| {
+                    *ty = qualify_generic(
+                        bound_generics,
+                        // Sad clone :(
+                        ty.clone(),
+                    );
+                });
+            }
+        };
+    });
+}
+
+/// Attempt to qualify the provided type if it's detected to be a generic.
+fn qualify_generic(bound_generics: &HashMap<Ident, Path>, mut ty: Type) -> Type {
+    let segments = match ty {
+        // Only accept `Path` types which aren't already qualified and don't have a leading `::`.
+        Type::Path(TypePath {
+            qself: None,
+            path:
+                Path {
+                    leading_colon: None,
+                    ref mut segments,
+                },
+        }) => segments,
+        // Try and recurse to resolve `qself`'s type.
+        Type::Path(TypePath {
+            qself: Some(mut qself),
+            mut path,
+        }) => {
+            qself.ty = Box::new(qualify_generic(bound_generics, *qself.ty));
+            qualify_path_segments(bound_generics, &mut path.segments.iter_mut());
+            return TypePath {
+                qself: Some(qself),
+                path,
+            }
+            .into();
+        }
+        // Return everything else as-is.
+        _ => {
+            return ty;
+        }
+    };
+
+    qualify_path_segments(bound_generics, segments.iter_mut());
+
+    // There must be two or more segments, otherwise it's just an identifier.
+    if segments.len() <= 1 {
+        return ty;
+    }
+
+    // Pull out the first ident, and see if it's bound to a trait.
+    let Some((ident, bound_trait)) = segments
+        .first()
+        .map(|segment| segment.ident.clone())
+        .and_then(|ident| {
+            let bound = bound_generics.get(&ident)?;
+            Some((ident, bound))
+        })
+    else {
+        return ty;
+    };
+
+    // Contruct a new punctuated list of segments, with the first removed.
+    let segments: Punctuated<_, Token![::]> =
+        std::mem::take(segments).into_iter().skip(1).collect();
+
+    parse_quote_spanned! { ty.span() => <#ident as #bound_trait>::#segments }
+}
+
+#[cfg(test)]
+mod test {
+    use quote::ToTokens;
+
+    use super::*;
+
+    fn test_qualify_generic(ty: Type, expected: Type) {
+        let bound_generics = [
+            (parse_quote!(T), parse_quote!(MyTrait)),
+            (parse_quote!(U), parse_quote!(some_module::OtherTrait)),
+        ]
+        .into();
+        let output = qualify_generic(&bound_generics, ty);
+        println!(
+            "output: {}, expected: {}",
+            output.to_token_stream(),
+            expected.to_token_stream()
+        );
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn single_ident() {
+        test_qualify_generic(parse_quote!(T), parse_quote!(T));
+    }
+
+    #[test]
+    fn associated_type() {
+        test_qualify_generic(parse_quote!(T::Item), parse_quote!(<T as MyTrait>::Item));
+    }
+
+    #[test]
+    fn nested_associated_type() {
+        test_qualify_generic(
+            parse_quote!(<T::Item as OtherTrait>::Item),
+            parse_quote!(<<T as MyTrait>::Item as OtherTrait>::Item),
+        );
+    }
+
+    #[test]
+    fn super_nested_associated_type() {
+        test_qualify_generic(
+            parse_quote!(<<T::Item as OtherTrait>::Item as OtherOtherTrait>::Item),
+            parse_quote!(<<<T as MyTrait>::Item as OtherTrait>::Item as OtherOtherTrait>::Item),
+        );
+    }
+
+    #[test]
+    fn as_other_generic() {
+        test_qualify_generic(
+            parse_quote!(SomeStruct<T::Item>),
+            parse_quote!(SomeStruct<<T as MyTrait>::Item>),
+        );
+    }
+
+    #[test]
+    fn associated_type_generic() {
+        test_qualify_generic(
+            parse_quote!(<K as TraitWithGeneric>::Item<T::Item>),
+            parse_quote!(<K as TraitWithGeneric>::Item<<T as MyTrait>::Item>),
+        );
+    }
 }
--- assert_layout/tests/simple.rs
+++ assert_layout/tests/simple.rs
@@ -1,35 +1,71 @@
 use assert_layout::assert_layout;
 
-// #[assert_layout(size = 9)]
-// #[repr(C, packed)]
-// struct MyStruct {
-//     #[assert_layout(size = 1, offset = 0)]
-//     a: u8,
-//     #[assert_layout(size = 4, offset = 1)]
-//     b: u32,
-//     c: u32,
-// }
-//
-// #[assert_layout(size = 9, generics = "u32")]
-// #[repr(C, packed)]
-// struct MyGenericStruct<T> {
-//     a: u8,
-//     b: u32,
-//     #[assert_layout(size = 4)]
-//     c: T,
-// }
-//
-// #[assert_layout(size = 6, generics = "u32, u8")]
-// #[repr(C, packed)]
-// struct MyDoubleGenericStruct<T, U> {
-//     #[assert_layout(offset = 0, size = 1)]
-//     a: u8,
-//     #[assert_layout(offset = 1, size = 4)]
-//     b: T,
-//     #[assert_layout(offset = 5, size = 1)]
-//     c: U,
-// }
+#[assert_layout(size = 9)]
+#[repr(C, packed)]
+struct MyStruct {
+    #[assert_layout(size = 1, offset = 0)]
+    a: u8,
+    #[assert_layout(size = 4, offset = 1)]
+    b: u32,
+    c: u32,
+}
+
+#[assert_layout(size = 9, generics = "u32")]
+#[repr(C, packed)]
+struct MyGenericStruct<T> {
+    a: u8,
+    b: u32,
+    #[assert_layout(size = 4)]
+    c: T,
+}
+
+#[assert_layout(size = 6, generics = "u32, u8")]
+#[repr(C, packed)]
+struct MyDoubleGenericStruct<T, U> {
+    #[assert_layout(offset = 0, size = 1)]
+    a: u8,
+    #[assert_layout(offset = 1, size = 4)]
+    b: T,
+    #[assert_layout(offset = 5, size = 1)]
+    c: U,
+}
 
 #[assert_layout(size = 123, generics = "123")]
 #[repr(C, packed)]
-struct ConstStruct<const N: usize>([u8; N]);
+struct ConstStruct<const N: usize>(#[assert_layout(size = 123)] [u8; N]);
+
+#[assert_layout(generics = "u16")]
+#[repr(C, packed)]
+struct GenericWithTrait<T: TheTrait> {
+    #[assert_layout(offset = 0, size = 2)]
+    item: T,
+    #[assert_layout(offset = 2, size = 4)]
+    nested: T::Item,
+}
+
+trait TheTrait {
+    type Item;
+}
+
+impl TheTrait for u16 {
+    type Item = u32;
+}
+
+#[assert_layout(generics = "u64")]
+#[repr(C, packed)]
+struct GenericWithNestedTrait<T: NestedTrait> {
+    #[assert_layout(offset = 0, size = 8)]
+    item: T,
+    #[assert_layout(offset = 8, size = 2)]
+    nested: T::Nested,
+    #[assert_layout(offset = 10, size = 4)]
+    deep_nested: <T::Nested as TheTrait>::Item,
+}
+
+trait NestedTrait {
+    type Nested: TheTrait;
+}
+
+impl NestedTrait for u64 {
+    type Nested = u16;
+}
